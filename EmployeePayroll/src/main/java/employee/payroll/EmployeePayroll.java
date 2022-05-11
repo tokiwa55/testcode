ab00baf5cdc0d4febe3e5b90d15b4b44844d6468
tokiwa55 <applicationtest824@gmail.com>
2022-05-11 16:34:58 +0900

    first commit

---
+++ EmployeePayroll/src/main/java/employee/payroll/EmployeePayroll.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package employee.payroll;
+
+/**
+ *
+ * @author tsuda
+ */
+public class EmployeePayroll {
+    public static void main(String[] args){
+   
+
+ }
+}
